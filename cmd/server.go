eb2b94ce9e2f3cf0ef35ed2c869d56882afe60cd
AlexandrPipko <a_pipko@mail.ru>
2023-12-19 21:47:23 +0300

    test

---
+++ cmd/server.go
@@ -0,0 +1,18 @@
+package main // Имя текущего пакета
+
+// Импорты других пакетов
+import "fmt"
+
+// Неявная инициализация пакета
+func init() {
+	fmt.Println("Hello from init!")
+}
+
+// Функция main как точка входа
+func main() {
+	foo()
+}
+
+func foo() {
+	fmt.Println("Foo!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/M1rm1/test
+
+go 1.21.5
